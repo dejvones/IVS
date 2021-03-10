deb607fc07c14c2a3f0dd6cbe38e0af67bdb8585
dejvones <d3jv.david@gmail.com>
2021-03-10 17:41:11 +0100

    Tvorba rozhrani pro matematickou knihovnu

---
+++ src/MathLib/IMath.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace MathLib
+{
+    /// <summary>
+    /// Rozhraní pro matematickou knihovnu
+    /// </summary>
+    public interface IMath
+    {
+        /// <summary>
+        /// Funkce počítající součet 2 čísel
+        /// </summary>
+        /// <param name="number1">Sčítanec 1</param>
+        /// <param name="number2">Sčítanec 2</param>
+        /// <returns>Vrací součet</returns>
+        double GetSum(double number1, double number2);
+
+        /// <summary>
+        /// Funkce počítající rozdíl 2 čísel
+        /// </summary>
+        /// <param name="number1">Menšenec</param>
+        /// <param name="number2">Menšitel</param>
+        /// <returns>Vrací rozdíl</returns>
+        double GetSub(double number1, double number2);
+
+        /// <summary>
+        /// Funkce počítající součin 2 čísel
+        /// </summary>
+        /// <param name="number1">Činitel 1</param>
+        /// <param name="number2">Činitel 2</param>
+        /// <returns>Vrací součin</returns>
+        double GetMul(double number1, double number2);
+
+        /// <summary>
+        /// Funkce počítající podíl 2 čísel
+        /// </summary>
+        /// <param name="number1">Dělenec</param>
+        /// <param name="number2">Dělitel</param>
+        /// <returns>Vrací podíl</returns>
+        double GetDiv(double number1, double number2);
+
+        /// <summary>
+        /// Funkce počítající faktoriál čísla
+        /// </summary>
+        /// <param name="number">Číslo faktoriálu</param>
+        /// <returns>Vrací výsledný faktoriál</returns>
+        ulong GetFactorial(double number);
+
+        /// <summary>
+        /// Funkce počítající N mocninu čísla
+        /// </summary>
+        /// <param name="basis">Základ mocniny</param>
+        /// <param name="exponent">Exponent</param>
+        /// <returns>Vrací výslednou mocninu</returns>
+        double GetPower(double basis, double exponent);
+
+        /// <summary>
+        /// Funkce počítající obecnou odmocninu
+        /// </summary>
+        /// <param name="basis">Základ odmocniny</param>
+        /// <param name="degree">Stupeň odmocniny</param>
+        /// <returns>Vrací výslednou odmocninu</returns>
+        double GetRoot(double basis, double degree);
+
+        /// <summary>
+        /// Funkce počítající dekadický logaritmus
+        /// </summary>
+        /// <param name="arg">Argument logaritmu</param>
+        /// <returns>Vrací výsledný logaritmus</returns>
+        double Log10(double arg);
+
+        /// <summary>
+        /// Funkce počítající přirozený logartimus
+        /// </summary>
+        /// <param name="arg">Argument logaritmu</param>
+        /// <returns>Vrací výsledný logaritmus</returns>
+        double LogE(double arg);
+    }
+}
---
+++ src/MathLib/Math.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLib
+{
+    /// <summary>
+    /// Implementace jednotlivých operací v matematické knihovně
+    /// </summary>
+    public class Math : IMath
+    {
+        public double GetSum(double number1, double number2)
+        {
+            return 0;
+        }
+        public double GetSub(double number1, double number2)
+        {
+            return 0;
+        }
+        public double GetMul(double number1, double number2)
+        {
+            return 0;
+        }
+        public double GetDiv(double number1, double number2)
+        {
+            return 0;
+        }
+
+        public ulong GetFactorial(double number)
+        {
+            return 0;
+        }
+
+        public double GetPower(double basis, double exponent)
+        {
+            return 0;
+        }
+
+        public double GetRoot(double basis, double degree)
+        {
+            return 0;
+        }
+
+        public double Log10(double arg)
+        {
+            return 0;
+        }
+
+        public double LogE(double arg)
+        {
+            return 0;
+        }
+    }
+}
