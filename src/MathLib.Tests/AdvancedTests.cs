84215385e2c8b246c1a904e0e4b766c462c4f337
dejvones <d3jv.david@gmail.com>
2021-03-10 17:41:38 +0100

    Napojeni testu na matematickou knihovnu

---
+++ src/MathLib.Tests/AdvancedTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace MathLib.Tests
+{
+    public class AdvancedTests
+    {
+        private readonly IMath math;
+
+        /// <summary>
+        /// Konstruktor pro pokročilé testy
+        /// </summary>
+        public AdvancedTests()
+        {
+            math = new Math();
+        }
+
+        [Fact]
+        public void GetSumTest()
+        {
+            math.GetFactorial(1);
+        }
+    }
+}
---
+++ src/MathLib.Tests/BasicTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace MathLib.Tests
+{
+    public class BasicTests
+    {
+        private readonly IMath math;
+
+        /// <summary>
+        /// Konstruktor pro základní testy
+        /// </summary>
+        public BasicTests()
+        {
+            math = new Math();
+        }
+
+        [Fact]
+        public void GetSumTest()
+        {
+            math.GetSum(1, 1);
+        }
+    }
+}
