232c668e78db2e9baec8114579bf7dd58603855d
gastoner <evzen.ml@seznam.cz>
2021-03-11 17:12:27 +0100

    Pridany Basic a Advanced testy

--- src/MathLib.Tests/AdvancedTests.cs
+++ src/MathLib.Tests/AdvancedTests.cs
@@ -16,9 +16,73 @@ namespace MathLib.Tests
         }
 
         [Fact]
-        public void GetSumTest()
+        public void GetFactorialTest()
         {
-            math.GetFactorial(1);
+            Assert.Equal(math.GetFactorial(5),Decimal.ToUInt64(120));
+            Assert.Equal(math.GetFactorial(1), Decimal.ToUInt64(1));
+            Assert.Equal(math.GetFactorial(0), Decimal.ToUInt64(1));
+            Assert.Equal(math.GetFactorial(20), Decimal.ToUInt64(2432902008176640000));
+            //TODO
+            //Assert.Equal(math.GetFactorial(0.5), Decimal.ToUInt64(0.88622692545));
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.GetFactorial(-1));
+            Assert.Throws<OverflowException>(() => math.GetFactorial(50));
+        }
+
+        [Fact]
+        public void GetPowerTest()
+        {
+            Assert.Equal(math.GetPower(42,0),1);
+            Assert.Equal(math.GetPower(0, 0), 1);
+            Assert.Equal(math.GetPower(0, 42), 0);
+            Assert.Equal(math.GetPower(2, 2), 4);
+
+            //Predpoklada se -(1)^0 == -1   (-1)^0 == 0 by bylo rozsireni
+            Assert.Equal(math.GetPower(-1, 0), -1);
+            Assert.Equal(math.GetPower(42, 1), 42);
+            Assert.Equal(math.GetPower(2, -1), 0.5);
+            Assert.Equal(math.GetPower(42, 0), 1);
+            Assert.Equal(math.GetPower(-0.5, -1), -2);
+            Assert.Throws<OverflowException>(() => math.GetPower(50,50));
+            //inf
+            Assert.Throws<OverflowException>(() => math.GetPower(0.0000005, -100)); 
+        }
+
+        [Fact]
+        public void GetRootTest()
+        {
+            Assert.Equal(math.GetRoot(2,100),10);
+            Assert.Equal(math.GetRoot(3, 8), 2);
+            Assert.Equal(math.GetRoot(1, 8), 8);
+            Assert.Equal(math.GetRoot(0, 8), 0);
+            Assert.Equal(math.GetRoot(2, 0), 0);
+            Assert.Equal(math.GetRoot(3, 1), 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.GetRoot(-1,42));
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.GetRoot(2, -42));
+
+        }
+
+        [Fact]
+        public void GetLog10Test()
+        { 
+            Assert.Equal(math.Log10(1),0);
+            Assert.Equal(math.Log10(5), 0.698970004336);
+            Assert.Equal(math.Log10(100), 2);
+            Assert.Equal(math.Log10(0.0001), -4);
+            Assert.Equal(math.Log10(2.5), 0.397940008672);
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.Log10(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.Log10(-1));
+        }
+
+        [Fact]
+        public void GetLogETest()
+        {
+            Assert.Equal(math.LogE(1), 0);
+            Assert.Equal(math.LogE(5), 0.698970004336);
+            Assert.Equal(math.LogE(100), 4.6051701859881);
+            Assert.Equal(math.LogE(0.0001), -9.2103403719762);
+            Assert.Equal(math.LogE(2.5), 0.91629073187416);
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.LogE(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => math.LogE(-1));
         }
     }
 }
--- src/MathLib.Tests/BasicTests.cs
+++ src/MathLib.Tests/BasicTests.cs
@@ -17,8 +17,62 @@ namespace MathLib.Tests
 
         [Fact]
         public void GetSumTest()
+        { 
+            Assert.Equal(math.GetSum(42, 42), 84);
+            Assert.Equal(math.GetSum(1, -1), 0);
+            Assert.Equal(math.GetSum(-1, -1), -2);
+            Assert.Equal(math.GetSum(0, 0), 0);
+            Assert.Equal(math.GetSum(0, -1), -1);
+            Assert.Equal(math.GetSum(-1, 0), -1);
+            Assert.Equal(math.GetSum(0, 1), 1);
+            Assert.Equal(math.GetSum(1, 0), 1);
+            Assert.Equal(math.GetSum(1.23456789,1.23456789), 2.46913578);
+            Assert.Equal(math.GetSum(1.23456789, -1.23456789),0);
+        }
+
+        [Fact]
+        public void GetSubTest()
         {
-            math.GetSum(1, 1);
+            Assert.Equal(math.GetSub(42,42),0);
+            Assert.Equal(math.GetSub(1, -1), 2);
+            Assert.Equal(math.GetSub(-1, 1), -2);
+            Assert.Equal(math.GetSub(-1, -1), 0);
+            Assert.Equal(math.GetSub(0, 0), 0);
+            Assert.Equal(math.GetSub(0,-1),1);
+            Assert.Equal(math.GetSub(-1, 0), -1);
+            Assert.Equal(math.GetSub(0, 1), -1);
+            Assert.Equal(math.GetSub(1, 0), 1);
+            Assert.Equal(math.GetSub(1.23456789, 1.23456789), 0);
+            Assert.Equal(math.GetSub(1.23456789, -1.23456789), 2.46913578);
+        }
+
+        [Fact]
+        public void GetMulTest()
+        {
+            Assert.Equal(math.GetMul(1,0), 0);
+            Assert.Equal(math.GetMul(0, 1), 0);
+            Assert.Equal(math.GetMul(1, 1), 1);
+            Assert.Equal(math.GetMul(1, -1), -1);
+            Assert.Equal(math.GetMul(-1, 1), -1);
+            Assert.Equal(math.GetMul(-1, -1), 1);
+            Assert.Equal(math.GetMul(0, 0), 0);
+            Assert.Equal(math.GetMul(1.23456789, 1.23456789), 1.524157875019052);
+            Assert.Equal(math.GetMul(1.23456789, -1.23456789), -1.524157875019052);
+        }
+
+        [Fact]
+        public void GetDivTest()
+        {
+            Assert.Equal(math.GetDiv(42, 42), 1);
+            Assert.Equal(math.GetDiv(1, -1), -1);
+            Assert.Equal(math.GetDiv(-1, 1), -1);
+            Assert.Equal(math.GetDiv(-1, -1), 1);
+            Assert.Equal(math.GetMul(0, 42), 0);
+            Assert.Equal(math.GetSub(1.23456789, 1.23456789), 1);
+            Assert.Equal(math.GetSub(1.23456789, -1.23456789), -1);
+            Assert.Throws<DivideByZeroException>(() => math.GetDiv(42,0));
+            Assert.Throws<DivideByZeroException>(() => math.GetDiv(0, 0));
+
         }
     }
 }
