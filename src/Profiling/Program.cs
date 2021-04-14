ffda3d60493226ed42b5a28013f43261422fcfb4
Stanislav Přikryl <xprikr33@vutbr.cz>
2021-04-14 16:50:43 +0200

    Provedeni profilingu

--- src/Profiling/Program.cs
+++ src/Profiling/Program.cs
@@ -2,32 +2,34 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using MathLib;
+using Math = MathLib.Math;
 
-
+/// <summary>
+/// Výběrová směrodatná odchylka, nad kterou byl proveden profiling
+/// </summary>
 namespace Profiling
 {
+    /// <summary>
+    /// Implementace profilingu
+    /// </summary>
     class Program
     {
-        private readonly MathLib.Math _math;
-        public Program() {
-            _math = new MathLib.Math();
-        }
-
+        /// <summary>
+        /// Hlavní funkce kódu
+        /// </summary>
+        /// <param name="args">Vstupní čísla pro výpočet výběrové směrodatné odchylky</param>
         static void Main(string[] args)
         {
-            Program prog = new Program();
-            prog.Rozptyl();
-        }
-
-        private void Rozptyl() {
-
+            Math _math = new Math();
             var numbers = GetNums();
-            double variance = GetVariance(numbers);
-            double standartDeviation = _math.GetRoot(2, variance);
+            double standartDeviation = GetDeviation(numbers, _math);
             Console.WriteLine(standartDeviation);
         }
 
+        /// <summary>
+        /// Získá čísla ze standartního vstupu a vloží je do seznamu
+        /// </summary>
+        /// <returns>Seznam čísel pro výpočet výběrové směrodatné odchylky</returns>
         public static List<string> GetNums()
         {
 
@@ -35,15 +37,25 @@ namespace Profiling
             string line;
             while (true){
                 line = Console.ReadLine();
-                if (line == null)  break;
+                if (line == null)
+                {
+                    break;
+                }
                 var num = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
                 numbers.AddRange(num);
             }
             return numbers;
         }
 
-        public double GetVariance(List<string> numbers)
+        /// <summary>
+        /// Samotný výpočet výběrové směrodatné odchylky
+        /// </summary>
+        /// <param name="numbers">Seznam čísel, ze kterého se vypočítá výběrová směrodatná odchylka</param>
+        /// <param name="_math">Instance matematické knihovny</param>
+        /// <returns></returns>
+        public static double GetDeviation(List<string> numbers, Math _math)
         {
+            double sub;
             double m = 0.0;
             double s = 0.0;
             double oldM = 0.0;
@@ -58,12 +70,14 @@ namespace Profiling
                     System.Environment.Exit(1);
                 }
                 oldM = m;
-                m = _math.GetSum(m, _math.GetDiv(_math.GetSub(x, m), i));
+                sub = _math.GetSub(x, m);
+                m = _math.GetSum(m, _math.GetDiv(sub, i));
                 s = _math.GetSum(s, _math.GetMul(_math.GetSub(x, m), _math.GetSub(x, oldM)));
                 i++;
             }
             double variance = _math.GetDiv(s, _math.GetSub(i, 1));
-            return variance;
+            double deviation = _math.GetRoot(2, variance);
+            return deviation;
         }
     }
 }
